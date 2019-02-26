ffee38248e3cd7f1ea42a7e16cd2d73b71ee0caf
J. Reynolds <jkr@udel.edu>
2019-02-25 22:04:40 -0500

    wrote a script to delete the nohup log each day so it doesnt balloon up

---
+++ PyServer/logWatch.py
@@ -0,0 +1,11 @@
+from datetime import datetime as dt
+import time
+import subprocess as sub
+
+while True:
+    print(dt.now().date())
+    time.sleep(3600)
+    try:
+        sub.call("rm nohup.out")
+    except:
+        pass
