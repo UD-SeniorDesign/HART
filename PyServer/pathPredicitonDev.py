1aaed4a84e4d4c3b43a3da422ae02894156c5faa
Jason Reynolds <jkr@udel.edu>
2019-03-13 14:33:09 -0400

    have functioning path prediction module

--- PyServer/pathPredicitonDev.py
+++ PyServer/pathPredicitonDev.py
@@ -12,6 +12,45 @@ history = [{ "Latitude" :40.79161316333334 , "Longitude" :-73.95754142916667 , "
 { "Latitude" :40.79149447000002 , "Longitude" :-73.95765686250002 , "Elevation" :100.0},
 { "Latitude" :40.79147963333335 , "Longitude" :-73.95767129166668 , "Elevation" :100.0}]
 
+history2 =[
+{ "Latitude" :40.79136093999805 , "Longitude" :-73.9577867249977 , "Elevation" :100.0},
+{ "Latitude" :40.79139061333138 , "Longitude" :-73.95775786666437 , "Elevation" :100.0},
+{ "Latitude" :40.79142028666471 , "Longitude" :-73.95772900833103 , "Elevation" :100.0},
+{ "Latitude" :40.79144995999804 , "Longitude" :-73.95770014999769 , "Elevation" :100.0},
+{ "Latitude" :40.79147963333137 , "Longitude" :-73.95767129166435 , "Elevation" :100.0},
+{ "Latitude" :40.7915093066647 , "Longitude" :-73.95764243333102 , "Elevation" :100.0},
+{ "Latitude" :40.79153897999803 , "Longitude" :-73.95761357499768 , "Elevation" :100.0},
+{ "Latitude" :40.79156865333136 , "Longitude" :-73.95758471666434 , "Elevation" :100.0},
+{ "Latitude" :40.79159832666469 , "Longitude" :-73.957555858331 ,  "Elevation" :100.0},
+{ "Latitude" :40.79162799999802 , "Longitude" :-73.95752699999767 , "Elevation" :100.0}]
+
+def getAvgDistance(arr):
+    difSum = 0
+    
+    for i in range(len(arr) - 1):
+        difSum += abs(arr[i]-arr[i+1])
+    
+    avgDif = difSum / len(arr)
+
+    return avgDif
+
+def getPoint(xCo,yCo,zCo,t,d):
+    predX = 0
+    predY = 0 
+    predZ = 0
+
+    # for i in range(d,0,-1):
+    for i in range(1,d+1):
+        predX += xCo[i]*t**(d-i)
+        predY += yCo[i]*t**(d-i)
+        predZ += zCo[i]*t**(d-i)
+        # print("i: " +str(i))
+        # print("d-1: " +str(d-i))
+        # print("xCoef: " + str(xCo[i]))
+        # print("yCoef: " + str(yCo[i]))
+        # print("zCoef: " + str(zCo[i]))
+        # print(predX,predY,predZ)
+    return predX,predY,predZ
 
 def pathPredict(locHist,numPnts):
     lats = []
@@ -25,32 +64,24 @@ def pathPredict(locHist,numPnts):
         longs.append(loc["Longitude"])
         elles.append(loc["Elevation"])
 
-    latDiffs = [abs(e[1] - e[0]) for e in itertools.permutations(lats, 2)]
-    # avgLatDiff = sum(latDiffs)/len(latDiffs)
-    avgLatDiff = np.std(latDiffs)
+    avgLatDiff = getAvgDistance(lats)
+    avgLngDiff = getAvgDistance(longs)
+    avgElDiff = getAvgDistance(elles)
 
-    lngDiffs = [abs(e[1] - e[0]) for e in itertools.permutations(longs, 2)]
-    # avgLngDiff = sum(lngDiffs)/len(lngDiffs)
-    avgLngDiff = np.std(lngDiffs)
-
-    elDiffs = [abs(e[1] - e[0]) for e in itertools.permutations(elles, 2)]
-    # avgElDiff = sum(elDiffs)/len(elDiffs)
-    avgElDiff = np.std(elDiffs)
 
     latDiffSign =  1 if ((lats[1] - lats[0]) > 0) else 0
     lngDiffSign = 1 if ((longs[1] - longs[0]) > 0) else 0
     elDiffSign = 1 if ((elles[1] - elles[0]) > 0) else 0
 
-    print("Averages")
+    # print("Averages")
 
-    print(avgLatDiff)
-    print(avgLngDiff)
-    print(avgElDiff)
+    # print(avgLatDiff)
+    # print(avgLngDiff)
+    # print(avgElDiff)
 
-    print(latDiffSign)
-    print(lngDiffSign)
-    print(elDiffSign)
-
+    # print(latDiffSign)
+    # print(lngDiffSign)
+    # print(elDiffSign)
 
     curPosition = locHist[len(locHist)-1]
 
@@ -74,8 +105,55 @@ def pathPredict(locHist,numPnts):
 
 future = pathPredict(history,10)
 
-for f in history:
-    print(str(f['Latitude']) + ',' + str(f['Longitude']) + "," + str(f['Latitude']) + ":" + str(f['Longitude']))
+# for f in history:
+#     print(str(f['Latitude']) + ',' + str(f['Longitude'])) # + "," + str(f['Latitude']) + ":" + str(f['Longitude'])
 
-for f in future:
-    print(str(f['Latitude']) + ',' + str(f['Longitude']) + "," + str(f['Latitude']) + ":" + str(f['Longitude']))
+# for f in future:
+#     print(str(f['Latitude']) + ',' + str(f['Longitude'])) # + "," + str(f['Latitude']) + ":" + str(f['Longitude'])
+
+
+def pathPredictPoly(locHist, numPnts):
+    lats = []
+    longs = []
+    elles = []
+    ticks = np.arange(len(locHist))
+
+    locPred = []
+    
+    for loc in locHist:
+        lats.append(loc["Latitude"])
+        longs.append(loc["Longitude"])
+        elles.append(loc["Elevation"])    
+
+    fitx = np.polyfit(ticks,lats,3)
+    fity = np.polyfit(ticks,longs,3)
+    fitz = np.polyfit(ticks,elles,3)
+
+    # print(ticks)
+    # print(fitx)
+    # print(fity)
+    # print(fitz)
+    return fitx,fity,fitz
+
+for f in history:
+    print(str(f['Latitude']) + ',' + str(f['Longitude'])) # + "," + str(f['Latitude']) + ":" + str(f['Longitude'])
+
+
+fX,fY,fZ = pathPredictPoly(history,10)
+
+for i in range(10,20):
+    ecks,whi,zee = getPoint(fX,fY,fZ,i,3)
+    print(ecks,end=", ")
+    print(whi)
+
+print("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
+
+for f in history2:
+    print(str(f['Latitude']) + ',' + str(f['Longitude'])) # + "," + str(f['Latitude']) + ":" + str(f['Longitude'])
+
+fX,fY,fZ = pathPredictPoly(history2,10)
+
+for i in range(10,20):
+    ecks,whi,zee = getPoint(fX,fY,fZ,i,3)
+    print(ecks,end=", ")
+    print(whi)
